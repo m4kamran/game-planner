93186e520412fdb09376b58d428af807ed312e23
Muhammad Kamran <m4kamran008@gmail.com>
2020-04-02 13:33:29 +0500

    Setup ant-deign boilerplate.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PlannerComponent } from './components/planner/planner.component';
+import { GeneratedPlanComponent } from './components/generated-plan/generated-plan.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/planner', pathMatch: 'full' },
+  {
+    path: 'planner',
+    component: PlannerComponent,
+    data: {
+      breadcrumb: 'Planner'
+    }
+  },
+  {
+    path: 'generated-plan',
+    component: GeneratedPlanComponent,
+    data: {
+      breadcrumb: 'Generated Plan'
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
