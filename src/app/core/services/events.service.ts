2fe4905c40ef660024386d68993b6c1f8b89e335
Muhammad Kamran <m4kamran008@gmail.com>
2020-04-02 18:49:56 +0500

    Create matches algo.

--- src/app/core/services/events.service.ts
+++ src/app/core/services/events.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { eachDayOfInterval, format } from 'date-fns';
+import { Router } from '@angular/router';
 
 
 @Injectable({
@@ -8,22 +9,13 @@ import { eachDayOfInterval, format } from 'date-fns';
 })
 export class EventsService {
 
-  private eventsSubject = new BehaviorSubject({
-    '02/04/2020': [
-      {
-        team1: ['kamran'],
-        team2: ['alex']
-      },
-      {
-        team1: ['kamran', 'lilly'],
-        team2: ['alex', 'jamine']
-      },
-    ],
-  });
+  private eventsSubject = new BehaviorSubject({});
 
   events$ = this.eventsSubject.asObservable();
 
-  constructor() { }
+  count = 0;
+
+  constructor(private router: Router) { }
 
   setEvents(events: any) {
     this.eventsSubject.next(events);
@@ -38,14 +30,43 @@ export class EventsService {
 
     allDates.forEach((date: Date) => {
       if (date.getDay().toString() === weekDay) {
+
         events[format(date, 'dd/MM/yyyy')] = this.getMatches(players);
       }
     });
 
     this.setEvents(events);
+    this.router.navigate(['generated-plan']);
   }
 
   getMatches(players: { player1: string, player2: string, player3: string, player4: string }) {
+    const allPlayers = [players.player1, players.player2, players.player3, players.player4];
+    const combinations = [];
 
+    allPlayers.forEach((p: string) => {
+      allPlayers.forEach((x: string) => {
+        if (p !== x) {
+          if (!combinations.find((c) => c[1] === p && c[0] === x)) {
+            combinations.push([p, x]);
+          }
+        }
+      });
+    });
+
+    const firstSingle = combinations[this.count % combinations.length];
+    const secondSingle = combinations.find((c) => {
+      if (!firstSingle.includes(c[0]) && !firstSingle.includes(c[1])) {
+        return c;
+      }
+    });
+
+    const double = [firstSingle, secondSingle];
+    this.count++;
+
+    return [
+      { team1: [firstSingle[0]], team2: [firstSingle[1]] },
+      { team1: [secondSingle[0]], team2: [secondSingle[1]] },
+      { team1: double[0], team2: double[1] }
+    ];
   }
 }
