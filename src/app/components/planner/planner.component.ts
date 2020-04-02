0e1165146d0ea3c2d8c8d0e704784ad0d7f91b15
Muhammad Kamran <m4kamran008@gmail.com>
2020-04-02 18:02:12 +0500

    Add planner component.

--- src/app/components/planner/planner.component.ts
+++ src/app/components/planner/planner.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { EventsService } from 'src/app/core/services/events.service';
 
 @Component({
   selector: 'app-planner',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlannerComponent implements OnInit {
 
-  constructor() { }
+  plannerForm: FormGroup;
+
+  submitForm(): void {
+    for (const i in this.plannerForm.controls) {
+      if (this.plannerForm.controls[i]) {
+        this.plannerForm.controls[i].markAsDirty();
+        this.plannerForm.controls[i].updateValueAndValidity();
+      }
+    }
+
+    this.eventService.generatePlan(this.plannerForm.value);
+  }
+
+  constructor(private fb: FormBuilder, private eventService: EventsService) { }
 
   ngOnInit(): void {
+    this.plannerForm = this.fb.group({
+      range: [[], [Validators.required]],
+      weekDay: [null],
+      player1: ['', Validators.required],
+      player2: ['', Validators.required],
+      player3: ['', Validators.required],
+      player4: ['', Validators.required],
+    });
   }
 
 }
